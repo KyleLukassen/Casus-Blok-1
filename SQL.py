fd873f4dfc278c6ef8859e421775695ed67ae070
Glenn-03 <glennvdh@live.nl>
2024-10-23 13:29:04 +0200

    Comments toegevoegd

--- SQL.py
+++ SQL.py
@@ -1,13 +1,20 @@
 import mysql.connector
+
+#Verbinding maken met de database
 mydb=mysql.connector.connect(
     host="localhost",
     user="root",
     password=input('Enter your database password: '),
+    database="mydatabase"
 )
+
+#cursor om door de database te kunnen lopen
 mycursor = mydb.cursor()
 
+#databases ophalen
 mycursor.execute("SHOW DATABASES")
 
+#print de inhoud van de database
 for x in mycursor:
   print(x)
 
